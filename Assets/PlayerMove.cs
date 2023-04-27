e4ae493bc83ccc5b4f589c52e39f58b441478db8
bleedinglight <halpin2003@gmail.com>
2023-04-27 12:24:53 +0100

    added grid movement and collision checks

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float gridSize = 1f;
+    private GameObject player;
+    private KeyCode moveUp = KeyCode.W;
+    private KeyCode moveDown = KeyCode.S;
+    private KeyCode moveRight = KeyCode.D;
+    private KeyCode moveLeft = KeyCode.A;
+    private BoxCollider2D boxCollider;
+    public LayerMask wall;
+
+    private bool canMoveUp = true;
+    private bool canMoveDown = true;
+    private bool canMoveRight = true;
+    private bool canMoveLeft = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<GameObject>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKeyDown(moveUp) && canMoveUp)
+        {
+            pos.y += gridSize;
+        }
+        if (Input.GetKeyDown(moveDown) && canMoveDown)
+        {
+            pos.y -= gridSize;
+        }
+        if (Input.GetKeyDown(moveRight) && canMoveRight)
+        {
+            pos.x += gridSize;
+        }
+        if (Input.GetKeyDown(moveLeft) && canMoveLeft)
+        {
+            pos.x -= gridSize;
+        }
+        transform.position = pos;
+        
+        checkForWalls();
+    }
+
+    void checkForWalls()
+    {
+        var upHit = Physics2D.Raycast(transform.position, Vector2.up, 0.5f, wall);
+        var downHit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, wall);
+        var rightHit = Physics2D.Raycast(transform.position, Vector2.right, 0.5f, wall);
+        var leftHit = Physics2D.Raycast(transform.position, Vector2.left, 0.5f, wall);
+        
+        if(upHit.collider != null)
+            canMoveUp = false;
+        else
+            canMoveUp = true;
+        
+
+        if (downHit.collider != null)
+            canMoveDown = false;
+        else
+            canMoveDown = true;
+        
+
+        if (rightHit.collider != null)
+            canMoveRight = false;
+        else
+            canMoveRight = true;
+        
+
+        if (leftHit.collider != null)
+            canMoveLeft = false;
+        else
+            canMoveLeft = true;
+        
+    }
+}
